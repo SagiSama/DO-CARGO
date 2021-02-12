3fa360732bfa16fed4618a6343457b69e10a9902
unisaruul <chinzorigt_saruulgun@unimedia.co.jp>
2021-02-12 14:55:08 +0800

    init

---
+++ app/javascript/packs/theme.js
@@ -0,0 +1,7 @@
+window.WOW = require('packs/wow.min').WOW
+window.classie = require('packs/classie')
+require('packs/pace.min')
+require('bootstrap')
+require('inspinia')
+require('packs/cbpAnimatedHeader')
+require('packs/gallery')
