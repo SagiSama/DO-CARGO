3f251b4e26e118de6ffef38050802a6dfefe8737
sagisama <mongolmonh@gmail.com>
2021-02-28 06:27:15 +0800

    landig fix begin

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,7 @@
 class LandingController < ApplicationController
   def landing_page
+    @top_sliders = Admin::Post.where(post_type: :top_slider)
+    @news = Admin::Post.where(post_type: :news)
   end
 
   def track_shipment
